76f060d9411baaa6d73f289937a57506f2202b2b
Boo <32691832+BanchouBoo@users.noreply.github.com>
2025-03-07 18:17:53 -0500

    Trackpad: add clarifying comments to config.h

--- ploopy-trackpad/config.h
+++ ploopy-trackpad/config.h
@@ -1,6 +1,13 @@
+// bigger data types for mouse reports, prevents overflow from acceleration
 #define MOUSE_EXTENDED_REPORT
+
+// disable automatic stylus detection
 #define MXT_ENABLE_STYLUS false
+// enable stylus device reporting, so I can manually send stylus inputs
 #define DIGITIZER_HAS_STYLUS true
+
+// number of contacts that the device can read, only the first 5 are
+// sent to the OS but the rest can be used internally
 #define DIGITIZER_CONTACT_COUNT 16
 
 // for printing floats
